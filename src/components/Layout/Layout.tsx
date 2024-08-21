e5d40b7d02288b173089a49406f1ed43e9bd4d02
rediska <sherstnev.denis.v@gmail.com>
2024-08-22 01:04:11 +0300

    frontend started

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,35 @@
+import styles from './Layout.module.scss';
+import { ThemeProvider, Button, Icon } from '@gravity-ui/uikit';
+import { Sun, Moon } from '@gravity-ui/icons';
+import { ReactNode, useState } from 'react';
+
+export interface WrapperProps {
+  children: ReactNode;
+}
+
+const DARK = 'dark';
+const LIGHT = 'light';
+const DEFAULT_THEME = DARK;
+
+export const Layout = (props: WrapperProps) => {
+
+  const [theme, setTheme] = useState<'light' | 'dark'>(DEFAULT_THEME);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          Axis
+        </div>
+        <Button onClick={() => setTheme(theme => theme === DARK ? LIGHT : DARK)} size={"l"} view='outlined'>
+          <Icon data={theme === DARK ? Sun : Moon} />
+        </Button>
+      </header>
+      <main className={styles.main}>
+        {
+          props.children
+        }
+      </main>
+    </ThemeProvider>
+  )
+}

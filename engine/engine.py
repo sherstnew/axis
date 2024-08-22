7069e91e4632e4cb59e954d7e93fab05dfa63c3e
Ivan Frolov <ivan-anatoliu@ya.ru>
2024-08-22 10:16:47 +0300

    skeleton of class

---
+++ engine/engine.py
@@ -0,0 +1,58 @@
+class Engine:
+    @staticmethod
+    def reculc_all_traffic(
+            construction_areas: list[any],
+            all_public_transport_stations: list[any],
+            all_roads_traffic: list[any],
+            region_params: any
+    ):
+
+
+    @staticmethod
+    def implement_region_params(
+            percent_of_working_people: int,
+            public_transport_usage: int,
+            traffic_to_center: int,
+            personal_transport_passenger_rate: int
+    ):
+        pass
+
+    @staticmethod
+    def calc_public_traffic(
+            new_public_transport_traffic: int,
+            all_public_transport_stations: list[any]
+    ):
+        pass
+
+    @staticmethod
+    def get_public_traffic_percentage(
+            stations_traffic: list[any]
+    ):
+        pass
+
+    @staticmethod
+    def calc_station_traffic(
+            old_traffic: int,
+            new_area_traffic: int
+    ):
+        pass
+
+    @staticmethod
+    def calc_roads_traffic(
+            new_road_transport_traffic: int,
+            all_roads_traffic: list[any]
+    ):
+        pass
+
+    @staticmethod
+    def get_road_traffic_percentage(
+            roads_traffic: list[any]
+    ):
+        pass
+
+    @staticmethod
+    def calc_road_traffic(
+            old_traffic: int,
+            new_area_traffic: int
+    ):
+        pass
